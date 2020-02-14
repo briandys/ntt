3e264e0a76442a0b7a999d4333533fdca597c8ca
Brian Dys <briansahagun@gmail.com>
2020-02-14 17:01:22 +0800

    Updates

--- includes/tags/entry-banner.php
+++ includes/tags/entry-banner.php
@@ -10,35 +10,34 @@ if ( ! function_exists( 'ntt_entry_banner' ) ) {
             ?>
             <div class="ntt--entry-banner ntt--cp" data-name="Entry Banner">
                 <?php
-                if ( get_the_post_thumbnail() !== '' ) {
-
-                    if ( is_singular() ) {
-                        $anchor_mu_start = '';
-                        $anchor_mu_end = '';
-                    } else {
-                        $anchor_mu_start = '<a href="'. esc_url( get_permalink() ). '" rel="bookmark" class="u-url">';
-                        $anchor_mu_end = '</a>';
-                    }
-                    ?>
-                    
-                    <div class="ntt--entry-banner-visuals ntt--obj" data-name="Entry Banner Visuals">
-                        <?php
-                        echo $anchor_mu_start;
-                            
-                        if ( is_singular() ) {
-                            $featured_image_size = 'ntt-large';
-                        } else {
-                            $featured_image_size = 'ntt-large';
-                        }
-
-                        the_post_thumbnail( apply_filters( 'ntt_entry_banner_visuals_featured_image_size_filter', $featured_image_size ), array( 'class' => 'u-featured', ) );
-                        
-                        echo $anchor_mu_end;
-                        ?>
-                    </div>
-                    <?php
+                if ( is_singular() ) {
+                    $anchor_mu_start = '';
+                    $anchor_mu_end = '';
+                } else {
+                    $anchor_mu_start = '<a href="'. esc_url( get_permalink() ). '" rel="bookmark" class="u-url">';
+                    $anchor_mu_end = '</a>';
                 }
                 ?>
+                
+                <figure class="ntt--entry-banner-visuals ntt--obj" data-name="Entry Banner Visuals">
+                    <?php
+                    if ( is_singular() ) {
+                        $featured_image_size = 'ntt-large';
+                    } else {
+                        $featured_image_size = 'ntt-large';
+                    }
+
+                    echo $anchor_mu_start;
+                    the_post_thumbnail( apply_filters( 'ntt_entry_banner_visuals_featured_image_size_filter', $featured_image_size ), array( 'class' => 'u-featured', ) );
+                    echo $anchor_mu_end;
+
+                    if ( get_the_post_thumbnail_caption() !== '' ) {
+                        ?>
+                        <figcaption><?php the_post_thumbnail_caption(); ?></figcaption>
+                    <?php
+                    }
+                    ?>
+                </figure>
             </div>
             <?php
         }
