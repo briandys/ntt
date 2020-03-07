f4f80fec7923e915f51fc1106d282b4293a0c075
Brian Dys <briansahagun@gmail.com>
2020-03-08 00:30:41 +0800

    Updates

--- includes/functions/styles-scripts.php
+++ includes/functions/styles-scripts.php
@@ -5,13 +5,13 @@
 
 function ntt__function__styles_scripts() {
 
-    wp_enqueue_style( 'ntt-style', get_template_directory_uri(). '/assets/styles/style.min.css', array(), wp_get_theme()->get( 'Version' ) );
+    wp_enqueue_style( 'ntt-style', get_template_directory_uri(). '/assets/styles/style.min.css', array(), wp_get_theme( get_template() )->get( 'Version' ) );
 
     wp_style_add_data( 'ntt-style', 'rtl', 'replace' );
     
     add_editor_style( array( 'assets/css/editor-style.css', ) );
 
-    wp_enqueue_script( 'ntt-script', get_template_directory_uri(). '/assets/scripts/main.js', array(), wp_get_theme()->get( 'Version' ), true );
+    wp_enqueue_script( 'ntt-script', get_template_directory_uri(). '/assets/scripts/main.js', array(), wp_get_theme( get_template() )->get( 'Version' ), true );
 
     if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
         wp_enqueue_script( 'comment-reply' );
@@ -48,7 +48,7 @@ if ( is_child_theme() ) {
     add_action('wp_enqueue_scripts', 'ntt__function__child_theme__styles', 0);
 
     function ntt__function__child_theme__css( $classes ) {
-        $classes[] = 'ntt--'. sanitize_title( $GLOBALS['ntt_child_theme_name'] );
+        $classes[] = 'ntt--'. sanitize_title( $GLOBALS['ntt__gvar__child_theme__name'] );
         return $classes;
     }
     add_filter( 'ntt_html_css_filter', 'ntt__function__child_theme__css' );
